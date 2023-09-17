3e4beec487399a11242c41b95b669ff8bc3bd9e3
ItsCtrax <ctrax.xaxa@gmail.com>
2023-09-17 14:00:09 +0300

    Create project

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-def hello():
-    print('hello students')
-hello()
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication,QVBoxLayout ,
+                             QHBoxLayout , QLabel ,  QWidget)
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Тестування')
+main_win.resize(400,400)
+
+main_win.show()
+app.exec_()
